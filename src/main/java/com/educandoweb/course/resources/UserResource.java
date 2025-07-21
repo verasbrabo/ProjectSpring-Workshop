3019209f65f85ba1266ebadaa3bb37bfb42e69f5
Caua <cauaveras93@gmail.com>
2025-07-21 16:50:10 -0300

    Procurando-Id

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -2,17 +2,30 @@ package com.educandoweb.course.resources;
 
 import com.educandoweb.course.entities.User;
 
+import com.educandoweb.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+
+    @Autowired
+    private UserService service;
+
       @GetMapping
-      public ResponseEntity<User>findAll(){
-          User u = new User(1L, "Maria", "maria@gamil.com", "999999999", "123456");
-          return ResponseEntity.ok().body(u);
+      public ResponseEntity<List<User>>findAll(){
+          List<User> list = service.findAll();
+
+          return ResponseEntity.ok().body(list);
       }
+
+      @GetMapping(value = "/{id}")
+      public ResponseEntity<User> findById(@PathVariable Long id){
+          User u = service.findById(id);
+          return ResponseEntity.ok().body(u);
+    }
 }
