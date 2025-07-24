ed106957b0de1fe00b562a001c1890c28e2eda85
Caua <cauaveras93@gmail.com>
2025-07-24 11:46:37 -0300

    Pacotes_Categoria

---
+++ src/main/java/com/educandoweb/course/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.educandoweb.course.repositories;
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//Não é obrigatorio colocar a anotation pois ela é herdada do JpaReposytory
+public interface CategoryRepository extends JpaRepository <Category, Long> {
+
+}
