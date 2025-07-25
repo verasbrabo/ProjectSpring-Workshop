637fafb18af96dc1d122f80bed4c80adef2d27ad
Caua <cauaveras93@gmail.com>
2025-07-25 14:43:41 -0300

    Inserindo-Usuarios

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -2,11 +2,15 @@ package com.educandoweb.course.resources;
 
 import com.educandoweb.course.entities.User;
 
+import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.services.UserService;
+import org.aspectj.apache.bcel.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -15,6 +19,7 @@ public class UserResource {
 
     @Autowired
     private UserService service;
+
 
       @GetMapping
       public ResponseEntity<List<User>>findAll(){
@@ -28,4 +33,12 @@ public class UserResource {
           User u = service.findById(id);
           return ResponseEntity.ok().body(u);
     }
+
+    @PostMapping
+    public ResponseEntity<User> insert (@RequestBody User obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return  ResponseEntity.created(uri).body(obj);
+    }
+
 }
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -22,4 +22,8 @@ public class UserService {
     public List<User> findAll(){
         return repository.findAll();
     }
+
+    public User insert (User obj){
+        return repository.save(obj);
+    }
 }
